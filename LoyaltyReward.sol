d18e1907c45a5cf93577c98b240e6260befe95b4
gunadi-w <84662544+gunadi-w@users.noreply.github.com>
2021-09-28 19:06:34 +0800

    Update LoyaltyReward.sol

--- LoyaltyReward.sol
+++ LoyaltyReward.sol
@@ -78,6 +78,7 @@ contract StarPoint {
     
     function reward(address _member, uint256 _value) public onlyPartner {
         require(balanceOf[msg.sender] >= _value,"not enough balance");
+        (members[_member].isMember, "address is not a member");
         balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
         balanceOf[_member] = balanceOf[_member] + _value;
         emit Reward(msg.sender, _member, _value);
@@ -118,3 +119,5 @@ contract StarPoint {
     
     
 }
+
+
